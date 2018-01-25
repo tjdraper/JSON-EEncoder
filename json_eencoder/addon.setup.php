9d4dc656e4f80c761f6efea2a55021f4d633b7c3
TJ Draper <tj@buzzingpixel.com>
2018-01-25 10:28:53 -0600

    Update files for current BuzzingPixel standards

--- json_eencoder/addon.setup.php
+++ json_eencoder/addon.setup.php
@@ -1,10 +1,16 @@
 <?php
 
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2018 BuzzingPixel, LLC
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 return array(
-	'author' => 'TJ Draper',
-	'author_url' => 'https://buzzingpixel.com',
-	'description' => 'JSON encode content.',
-	'name' => 'JSON EEncoder',
-	'namespace' => 'BuzzingPixel\Addons\JsonEEncoder',
-	'version' => '2.0.0'
+    'author' => 'TJ Draper',
+    'author_url' => 'https://buzzingpixel.com',
+    'description' => 'JSON encode content.',
+    'name' => 'JSON EEncoder',
+    'namespace' => 'BuzzingPixel\Addons\JsonEEncoder',
+    'version' => '2.0.0'
 );
